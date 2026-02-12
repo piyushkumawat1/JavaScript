4ba2268be392c3878ecf9efa8abef26604cf58e6
piyushkumawat1 <piyushkumawat3440@gmail.com>
2026-02-13 00:00:09 +0530

    nothing

--- Code.js
+++ Code.js
@@ -1,12 +1,7 @@
-class Student {
-    constructor(name,year,branch,section) {
-        console.log("Student information");
-        this.college = "Poornima college of Engineering";
-        this.name = name;
-        this.year= year;
-        this.branch= branch;
-        this.section =section;
-    }
-};
-
-let student1 = new Student("Piyush Kumawat","1st",'AIDS',"J");
+console.log("one");
+console.log("two");
+setTimeout(()=> {
+    console.log("hello");
+},5000);
+console.log("Three");
+console.log("Four");
