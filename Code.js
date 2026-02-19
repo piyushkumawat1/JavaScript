c0b7b6915d4c2924b1a802a341720ada93e2e435
piyushkumawat1 <piyushkumawat3440@gmail.com>
2026-02-19 22:20:30 +0530

    Events

--- Code.js
+++
@@ -1,6 +0,0 @@
-let newBtn = document.createElement("button");
-newBtn.innerText = "Click me!";
-let divs = document.querySelector("body");
-newBtn.style.backgroundColor = "red";
-newBtn.style.color = "white";
-divs.before(newBtn);
