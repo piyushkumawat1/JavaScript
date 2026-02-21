c4ffa24b40031dc1b3da0490de3ca9040e952af8
piyushkumawat1 <piyushkumawat3440@gmail.com>
2026-02-21 16:41:42 +0530

    promise chain

--- Code.js
+++ Code.js
@@ -1,11 +1,26 @@
-function getData(getId,getNextData){
+function asyncFun1(){
     return new Promise((resolve,reject) =>{
-        setTimeout(()=>{
-            console.log("data =",getId);
-            resolve("Success");
-            if(getNextData){
-                getNextData();
-            }
-        },5000);
+            setTimeout(()=>{
+                console.log("data1");
+                resolve("success");
+            },4000);
     });
 }
+
+function asyncFun2(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("data2");
+            resolve("success");
+        },4000);
+    });
+
+}
+
+console.log("Data1 fetching......")
+let p1 = asyncFun1();
+p1.then(()=>{
+    console.log("Data2 Fetching.....")
+    let p2 = asyncFun2();
+    p2.then(()=>{})
+});
